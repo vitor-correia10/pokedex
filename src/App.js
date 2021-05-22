d73f399a927e5ff3abb8413516822702566930bc
Vitor Correia <vitorbaziliogcorreia@gmail.com>
2021-05-22 08:48:11 -0400

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+    </Router>
+  );
+}
+
+export default App;
